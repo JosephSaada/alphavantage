619c85749f026c899085bc9e1539d2bd34cce28a
Joseph Saada <jsaada2003@gmail.com>
2022-06-05 02:23:28 -0700

    gets data yay!

--- main.py
+++ main.py
@@ -22,17 +22,31 @@ with requests.Session() as s:
 
 def print_time(result: csv.DictReader) -> None:
     prev = '2022-06-03'
+    clock = None
     price = 0
     for row in result:
         time = row["time"].split(" ")
         date = time[0]
         clock = time[1]
 
-        print(row)
+        if clock == '09:30:00' or clock == '16:00:00':
+            if clock == '09:30:00':
+                open = row["open"]
+                print(f"{date} {clock} {open} ", end='')
+            if clock == '16:00:00':
+                close = row["close"]
+                print(f"{date} {clock} {close} ", end='')
+                try:
+                    dif = (float(close) - float(open))
+                    diff = lambda x: True if x > 0 else False
+                    print(f'{diff(dif)} ')
+                except UnboundLocalError:
+                    print()
 
-        #if prev != date:
-         #   prev = date
-          #  print(date)
+        # if prev != date:
+        #   prev = date
+        #  print(date)
+        # print(clock)
 
 
 print_time(cr)
